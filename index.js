86172fabc781b658c5877147ef448ab3407ebbe0
francamateus93 <francamateus@gmail.com>
2024-10-21 17:21:17 +0200

    hero responsive

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-  document.getElementById('submitButton').addEventListener('click', function (event) {
-  event.preventDefault();
 
-  const emailInput = document.getElementById('emailInput');
-  const errorMessage = document.getElementById('errorMessage');
+// Menu Toggle
 
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const menuToggle = document.getElementById('menuToggle');
+const menuOverlay = document.getElementById('menuOverlay');
+const navbarNav = document.getElementById('navbarNav');
+const navLinks = document.querySelectorAll('.nav-link');
 
-  // Comprueba si el campo está vacío o el correo electrónico no tiene el formato correcto
-  if (emailInput.value.trim() === '') {
+menuToggle.addEventListener('click', function() {
+menuOverlay.style.display = menuOverlay.style.display === 'block' ? 'none' : 'block';
+  if (menuOverlay.style.display === 'block') {
+    navbarNav.style.display = 'none';
+  } else {
+    navbarNav.style.display = '';
+  }
+});
+
+document.getElementById('logoutButton').addEventListener('click', function() {
+menuOverlay.style.display = 'none';
+
+});
+
+navLinks.forEach(link => {link.addEventListener('click', function() {
+  menuOverlay.style.display = 'none';
+  });
+});
+
+// Input Form
+
+document.getElementById('submitButton').addEventListener('click', function (event) {
+event.preventDefault();
+  
+const emailInput = document.getElementById('emailInput');
+const errorMessage = document.getElementById('errorMessage');
+  
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  
+// Comprueba si está vacío o el correo electrónico no tiene el formato correcto
+if (emailInput.value.trim() === '') {
     errorMessage.textContent = 'Introduce una dirección de correo electrónico.';
     errorMessage.classList.remove('d-none');
   } else if (!emailPattern.test(emailInput.value)) {
@@ -17,4 +46,4 @@
     errorMessage.classList.add('d-none');
     alert('¡Formulario enviado exitosamente!');
   }
-});
+  });
